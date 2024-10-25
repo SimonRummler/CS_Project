27fda5c8e8b1b4b4e8c818befcab7e7044ec5a92
SimonRummler <simongeorg.rummler@student.unisg.ch>
2024-10-25 13:02:34 +0200

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -2,10 +2,10 @@ import gspread
 from google.oauth2.service_account import Credentials
 import streamlit as st
 
-# Pfad zur Dienstkonto-JSON-Datei (passe diesen Pfad an, wenn nötig)
-SERVICE_ACCOUNT_FILE = '/mnt/data/round-seeker-439709-p8-b1cf9a6ceb00.json'
+# Pfad zur Dienstkonto-JSON-Datei
+SERVICE_ACCOUNT_FILE = 'your-service-account-file.json'
 
-# Google Sheets API-Scopes (Berechtigungen) festlegen
+# Google Sheets API-Scopes (Berechtigungen)
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 # Anmeldedaten aus der JSON-Datei laden
@@ -20,9 +20,9 @@ spreadsheet = client.open('HR-Data')
 # Wähle das erste Blatt (Sheet1) aus
 sheet = spreadsheet.sheet1
 
-# Alle Daten als Liste von Dictionaries abrufen
+# Alle Daten abrufen
 data = sheet.get_all_records()
 
-# Die abgerufenen Daten in der Streamlit App anzeigen
+# Daten in der Streamlit-App anzeigen
 st.write("Daten aus dem Google Sheet:")
 st.write(data)
