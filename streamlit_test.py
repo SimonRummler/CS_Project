c1d74af3512cf76a992698a37f85ae2dada40378
SimonRummler <simongeorg.rummler@student.unisg.ch>
2024-10-25 13:21:00 +0200

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -2,14 +2,9 @@ import gspread
 from google.oauth2.service_account import Credentials
 import streamlit as st
 
-# Pfad zur Dienstkonto-JSON-Datei
-SERVICE_ACCOUNT_FILE = 'your-service-account-file.json'
-
-# Google Sheets API-Scopes (Berechtigungen)
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-
-# Anmeldedaten aus der JSON-Datei laden
-creds = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+# Dienstkonto-Informationen aus Streamlit Secrets laden
+service_account_info = st.secrets["gcp_service_account"]
+creds = Credentials.from_service_account_info(service_account_info)
 
 # Google Sheets Client erstellen
 client = gspread.authorize(creds)
